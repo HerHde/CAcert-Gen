0011c576f384f9e31034dbd506a3ff9bfd403cd5
HerHde <admin@herh.de>
2015-11-04 23:53:26 +0100

    Renamed variables.

--- gen.sh
+++ gen.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -i
-CAT=0
-TLD=""
+unset opt_cat opt_tld host_name
 while :
 do
     case "$1" in
       -d | --domain)
-        TLD="$2" && echo "--domain: \"."$TLD\" "will be appended to the domain names." >&2
+        opt_tld="$2" && echo "--domain: \"."$opt_tld\" "will be appended to the domain names." >&2
         shift 2
       ;;
       -h | --help)
@@ -16,15 +15,15 @@ do
       -c | --cat)
         case "$2" in
             3)
-                CAT=$(cat root.crt class3.crt) && echo "--cat: CAcert class 1 & 3 PKI certificates will be chained to each certificate." >&2
+                opt_cat=$(cat root.crt class3.crt) && echo "--cat: CAcert class 1 & 3 PKI certificates will be chained to each certificate." >&2
                 shift
             ;;
             1)
-                CAT=$(cat root.crt) && echo "--cat: CAcert class 1 PKI certificate will be chained to each certificate." >&2
+                opt_cat=$(cat root.crt) && echo "--cat: CAcert class 1 PKI certificate will be chained to each certificate." >&2
                 shift
             ;;
             *)
-                CAT=$(cat root.crt) && echo "--cat: CAcert class 1 PKI certificate will be chained to each certificate." >&2
+                opt_cat=$(cat root.crt) && echo "--cat: CAcert class 1 PKI certificate will be chained to each certificate." >&2
             ;;
         esac
         shift
@@ -46,22 +45,22 @@ done
 
 mkdir -p _output
 cd _output
-for ARG in $*
+for domain_name in $*
 do
-  if [ "$TLD" ]; then # If --domain is set
-      if [ "$ARG" == "." ] || [ "$ARG" == "$TLD" ]; then # If Hostname is = --domain
-          HOST=$TLD
+  if [ "$opt_tld" ]; then # If --domain is set
+      if [ "$domain_name" == "." ] || [ "$domain_name" == "$opt_tld" ]; then # If Hostname is = --domain
+          host_name=$opt_tld
       else
-          HOST=$ARG"."$TLD
+          host_name=$domain_name"."$opt_tld
       fi
-  else # Else (if HOST=FQND)
-      HOST=$ARG
+  else # Else (if host_name=FQND)
+      host_name=$domain_name
   fi
-  echo -e "\n=== DO $HOST:"
-  mkdir $HOST 2>/dev/null
+  echo -e "\n=== DO $host_name:"
+  mkdir $host_name 2>/dev/null
   if [ $? -ne 0 ]; then
     while true; do
-      read -p "A folder named $HOST already exists. Do you want to overwrite its content? (y/N): " yn
+      read -p "A folder named $host_name already exists. Do you want to overwrite its content? (y/N): " yn
       case $yn in
         [Yy]* ) break;;
         * ) echo "Will not overwrite. Exiting."; exit; break;;
@@ -70,7 +69,7 @@ do
   fi
 
 
-  cd $HOST
+  cd $host_name
 
   # Config file gen
   cat <<EOF > openssl.cnf
@@ -80,28 +79,28 @@ oid_section             = new_oids
 [ req ]
 default_days            = 730            # how long to certify for
 distinguished_name      = req_distinguished_name
-default_keyfile         = ${HOST}.key
+default_keyfile         = ${host_name}.key
 encrypt_key             = no
-string_mask = nombstr
+string_mask             = nombstr
 [ req_distinguished_name ]
 commonName              = Common Name (eg, YOUR name)
-commonName_default      = $HOST
+commonName_default      = $host_name
 # -------------- END custom openssl.cnf -----
 EOF
-  openssl req -new -batch -config openssl.cnf -newkey rsa:4096 -out $HOST.csr
-  chmod 600 $HOST.key
+  openssl req -new -batch -config openssl.cnf -newkey rsa:4096 -out $host_name.csr
+  chmod 600 $host_name.key
   rm openssl.cnf
   echo -e "\n Copy the following Certificate Request and paste it into the CAcert webformular to obtain a Certificate."
-  cat $HOST.csr
+  cat $host_name.csr
 
   echo "Copy the certificate shown on the Website, paste it here and press CTRL + D to send an end-of-file."
   CRTCHK=1
   until [ $CRTCHK -eq 0 ]; do
-    cat > $HOST.crt
-    sed -i -n '/^-----BEGIN CERTIFICATE-----/,$p' $HOST.crt # Delete anything before the BEGIN-line.
-    sed -i '/-----END CERTIFICATE-----/q' $HOST.crt # Delete anything after the END-line.
-    # sed -i -e '$a\' $HOST.crt
-    openssl verify -CAfile ../../root.crt $HOST.crt
+    cat > $host_name.crt
+    sed -i -n '/^-----BEGIN CERTIFICATE-----/,$p' $host_name.crt # Delete anything before the BEGIN-line.
+    sed -i '/-----END CERTIFICATE-----/q' $host_name.crt # Delete anything after the END-line.
+    # sed -i -e '$a\' $host_name.crt
+    openssl verify -CAfile ../../root.crt $host_name.crt
     CRTCHK=$?
     if [ $CRTCHK -ne 0 ]; then
       echo "Gooby, please reenter the *correct* certificate!"
@@ -109,12 +108,13 @@ EOF
     fi
   done
 
-  if [ "$CAT" != 0 ]; then
-      echo "$CAT" >> $HOST.crt
+  if [ "$opt_cat" != 0 ]; then
+      echo "$opt_cat" >> $host_name.crt
   fi
-  chmod 644 $HOST.crt
-  rm $HOST.csr
+  chmod 644 $host_name.crt
+  rm $host_name.csr
   ls -lA .
   cd ../
-  echo "=== DONE with $HOST"
+  echo "=== DONE with $host_name"
+  unset host_name
 done
